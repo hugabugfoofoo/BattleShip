5efa1da3a48dc7dab28a6f6a387fbc558f358f0b
Noah <hugabug@umich.edu>
2025-04-04 11:23:38 -0400

    Battleship with computer attack phase

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,24 @@
 #include <ctime>
 using namespace std;
 
-const int rows =10;
-const int elements =10;
-int maxships =10;
+const int rows =5;
+const int elements =5;
+int maxships = 5;
 int matrix[rows][elements];
-
+const int prows=5;
+const int pelements=5;
+int pmatrix[prows][pelements];
 
 void Clear() {
     for ( int i=0; i < rows; i++) {
         for ( int j=0; j < elements; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+}
+void Clearpl() {
+    for ( int i=0; i < prows; i++) {
+        for ( int j=0; j < pelements; j++) {
             matrix[i][j] = 0;
         }
     }
@@ -36,9 +45,35 @@ void SetShips() {
     }
 }
 
+void Showpl() {
+    for ( int i=0; i < prows; i++) {
+        for ( int j=0; j < pelements; j++) {
+            cout << pmatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void SetShipspl() {
+    int s=0;
+    while (s< maxships) {
+        int x =rand() % prows;
+        int y = rand() % pelements;
+        if (pmatrix[x][y] != 1) {
+            s++;
+            pmatrix[x][y] = 1;
+        }
+    }
+}
+bool PAttack(int x, int y) {
+if (pmatrix[x][y] == 1) {
+    pmatrix[x][y] = 0;
+return true;
+}
+return false;
+}
 bool Attack(int x, int y) {
     if (matrix[x][y] == 1){
-        matrix[x][y] = 2;
+        matrix[x][y] = 0;
         return true;
     }
     return false;
@@ -54,34 +89,68 @@ int NumberOfShips() {
     }
     return c;
 }
+int PNumberOfShips() {
+    int c=0;
+    for ( int i=0; i < prows; i++) {
+        for ( int j=0; j < pelements; j++) {
+            if (matrix[i][j]==1) {
+                c++;
+            }
+        }
+    }
+    return c;
+}
 
 
 int main() {
     srand(time(NULL));
-Clear();
+    Clear();
+    Clearpl();
     SetShips();
-int pos1, pos2;
+    //Show();
+    SetShipspl();
+    cout<<"Your Ships:"<< endl;
+    Showpl();
+
+    int pos1, pos2;
     char prompt;
+
     while (1) {
         cout <<"Please input attack coordinates:";
         cin >> pos1 >> pos2;
         if (Attack(pos1,pos2)) {
             cout <<"Hit!" << endl;
-            cout << "Ships left: " << NumberOfShips() << endl;
+            cout << "Enemy ships left: " << NumberOfShips() << endl;
         }else {
             cout <<"Miss" << endl;
-            cout << "Ships left: " << NumberOfShips() << endl;
+            cout << "Enemy ships left: " << NumberOfShips() << endl;
             cout << "do you want to surrender(y/n)?";
             cin >> prompt;
         }
-if (prompt == 'y') {
-    cout << "Game Over!" << endl;
-        Show();
-    break;
+        if (prompt == 'y') {
+            cout << "Game Over!" << endl;
+            Show();
+            break;
+        }
+           if (PAttack(rand()%prows,rand()%pelements)) {
+               cout<<"You've been hit!"<<endl;
+               cout << "Ships left: " << PNumberOfShips() << endl;
+               Showpl();
+           } else {
+               cout<<"The enemy missed!"<<endl;
+               cout << "Ships left: " << PNumberOfShips() << endl;
+           }
+        int loose = PNumberOfShips();
+        int win = NumberOfShips();
+        if (win==0) {
+            cout<<"YOU WIN!"<<endl;
+            break;
 
-}
-
+        }
+        if (loose==0) {
+            cout<<"YOU LOSE!"<<endl;
+            break;
+        }
+        }
     }
-    //system("pause");
-    return 0;
-}
+
